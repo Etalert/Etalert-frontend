c574e3ba93fa51346ec0b273aabf74b73e242732
wtyaumm <muaymiimuay@gmail.com>
2024-10-27 20:18:11 +0700

    feat: add loading to weekly report screen

--- lib/screens/routine_report.dart
+++ lib/screens/routine_report.dart
@@ -19,11 +19,13 @@ class RoutineReport extends StatefulWidget {
 
 class _RoutineReportState extends State<RoutineReport> {
   List<WeeklyReport> weeklyReport = [];
+  bool isLoading = true;
 
   getWeeklyReportData(String googleId, String startDate) async {
     var report = await getWeeklyReport(googleId, startDate);
     setState(() {
       weeklyReport = report!;
+      isLoading = false;
     });
   }
 
@@ -46,71 +48,77 @@ class _RoutineReportState extends State<RoutineReport> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(16)),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(255, 217, 217, 217)
-                        .withOpacity(0.5),
-                    spreadRadius: 1,
-                    blurRadius: 7,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                child: Row(
-                  children: [
-                    Icon(Icons.calendar_today_rounded,
-                        color: Theme.of(context).colorScheme.primary),
-                    const SizedBox(width: 12),
-                    Text('${widget.startDate} - ${widget.endDate}',
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500)),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-              child: Row(
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
                 children: [
-                  Text(
-                    'Routines Report',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
+                  Container(
+                    margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 217, 217, 217)
+                              .withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
                     ),
-                    textAlign: TextAlign.left,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                      child: Row(
+                        children: [
+                          Icon(Icons.calendar_today_rounded,
+                              color: Theme.of(context).colorScheme.primary),
+                          const SizedBox(width: 12),
+                          Text('${widget.startDate} - ${widget.endDate}',
+                              style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Routines Report',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSecondary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ...weeklyReport.map((e) {
+                            return RoutineReportDropdown(
+                              weeklyReport: e,
+                            );
+                          }).toList(),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ...weeklyReport.map((e) {
-                      return RoutineReportDropdown(
-                        weeklyReport: e,
-                      );
-                    }).toList(),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
